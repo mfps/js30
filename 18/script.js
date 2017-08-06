fddf2e898e8365b00926853f8e005e39b44c069f
mfps <f.s.wille@gmail.com>
2017-08-06 12:53:11 +0200

    update

--- 18/script.js
+++ 18/script.js
@@ -1,21 +1,40 @@
-const timeNodes = [...document.querySelectorAll('[data-time]')];
+class TallyStringTimes {
+  constructor(nodes) {
+    this.timeNodes = [...nodes];
+    this.seconds = '';
+    this.render();
+  }
 
-const seconds = timeNodes
-  .map(node => node.dataset.time)
-  .map(timecode => {
-    const [min, sec] = timecode.split(':').map(parseFloat);
-    return min * 60 + sec;
-  })
-  .reduce((total, seconds) => total + seconds);
+  getTime() {
+    return (this.seconds = this.timeNodes
+      .map(node => node.dataset.time)
+      .map(timecode => {
+        const [min, sec] = timecode.split(':').map(parseFloat);
+        return min * 60 + sec;
+      })
+      .reduce((total, seconds) => total + seconds));
+  }
 
-let secondsLeft = seconds;
-const hours = Math.floor(secondsLeft / 3600);
-secondsLeft = secondsLeft % 3600;
-const mins = Math.floor(secondsLeft / 60);
-secondsLeft = secondsLeft % 60;
+  formatTime() {
+    let seconds = this.getTime();
+    let secondsLeft = seconds;
+    const hours = Math.floor(secondsLeft / 3600);
+    secondsLeft = secondsLeft % 3600;
+    const mins = Math.floor(secondsLeft / 60);
+    secondsLeft = secondsLeft % 60;
+    return [hours, mins, secondsLeft, seconds];
+  }
 
-const time = `${hours}:${mins}:${secondsLeft}`;
-const total = document.querySelector('#total');
-total.innerHTML = time;
-total.setAttribute('data-total', seconds);
-console.log(hours, mins, secondsLeft);
+  render() {
+    const [hours, mins, secondsLeft, seconds] = this.formatTime();
+    const time = `${hours}:${mins}:${secondsLeft}`;
+    const total = document.querySelector('#total');
+    total.innerHTML = time;
+    total.setAttribute('data-total', seconds);
+  }
+}
+
+(() => {
+  const nodes = document.querySelectorAll('[data-time]');
+  new TallyStringTimes(nodes);
+})();
